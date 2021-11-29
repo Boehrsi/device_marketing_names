2973650d746bfd1e5ae57104565da18b7a8aae61
Boehrsi <git@boehrsi.de>
2021-11-29 16:34:48 +0100

    chore: formatting

--- lib/device_marketing_names.dart
+++ lib/device_marketing_names.dart
@@ -11,9 +11,11 @@ export 'src/device_lookup.dart' show DeviceType;
 class DeviceMarketingNames {
   /// Loads the marketing name for the current device. Internally the device model is loaded and matched against a list of known devices.
   /// For web browsers the browser name delivered by device_info_plus is forwarded.
-  Future<String?> getMarketingName() async => await lookupDevice(PlatformInfo(), DeviceInfo());
+  Future<String?> getMarketingName() async =>
+      await lookupDevice(PlatformInfo(), DeviceInfo());
 
   /// Loads the marketing name for an already known device model. The device model is matched against a list of known devices.
   /// Supported device types are Android and iOS.
-  String? getMarketingNameFromModel(DeviceType type, String model) => lookupName(type, model);
+  String? getMarketingNameFromModel(DeviceType type, String model) =>
+      lookupName(type, model);
 }
