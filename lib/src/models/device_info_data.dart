6f221fae22bea731a2b297f088606a362231b105
Boehrsi <git@boehrsi.de>
2026-04-02 12:11:20 +0200

    fix: Compatible with dependency constraint lower bounds

--- lib/src/models/device_info_data.dart
+++ lib/src/models/device_info_data.dart
@@ -1,7 +1,9 @@
+import 'package:device_marketing_names/src/logic/base/device_info_fallback.dart';
+
 class DeviceInfoData {
   final String model;
 
   const DeviceInfoData({
-    required this.model,
-  });
+    String? model,
+  }) : model = model ?? unknown;
 }
